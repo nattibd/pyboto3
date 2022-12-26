e73dc5510342312d005c259c2733f99ff713e9f6
Natyra DeGraffenreid <85776716+nattibd@users.noreply.github.com>
2022-12-25 20:19:50 -0700

    Update project0.py

--- project0.py
+++ project0.py
@@ -1,3 +1,12 @@
-"""
-Your module description
-"""
+
+import boto3
+
+#Instances
+Ids=['i-01f2b06f0bb44b046' , 'i-0428cdfe81928631d' , 'i-009afda57de57ba5d']
+
+ec2 = boto3.resource("ec2")
+
+x = ec2.instances.filter(InstanceIds=Ids).stop()
+
+print("Instance Stopped")
+print(x)
